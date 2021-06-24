b056021d18eabe3ecffa7c84083f363f5d04949a
JTMonitoring <86123318+JTMonitoring@users.noreply.github.com>
2021-06-23 18:58:50 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+
+nickname = input("Nickname> ")
+
+#start socket and connect
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 55555))
+
+def recieve():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occured.")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
